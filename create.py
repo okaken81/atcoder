cdf858f427948d783c07c60ed5d0db6f4e4f0782
kenji <kenji@kenjinoMacBook-Air.local>
2021-05-02 00:42:25 +0900

    up

--- create.py
+++ create.py
@@ -3,9 +3,9 @@ def main():
     import os
     import shutil
 
-    main_title = 'abc180'
-    sub_title = ['a', 'b', 'c']
-    # sub_title = ['d']
+    main_title = 'abc190'
+    # sub_title = ['a', 'b', 'c']
+    sub_title = ['d']
 
     dir = './atcoder/'
 
